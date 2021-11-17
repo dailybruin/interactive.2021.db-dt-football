616f304c18c0714230b8fa02dc63c56ad6917e24
Maggie Li <maggieelli@ucla.edu>
2021-11-16 23:11:52 -0800

    Edited formatting of text in box using flexbox, added fonts

--- src/components/Grid2ArticleBox.js
+++ src/components/Grid2ArticleBox.js
@@ -2,30 +2,36 @@ import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 
 const Box = styled.div`
-display: grid;
-grid-template-columns: 40% 60%;
+display: flex;
+flex-direction: row;
 height: 95%;
 width: 85%;
-justify-self: center;
+justify-content: center;
+align-items: center;
 `;
 
 const Text = styled.div`
-text-align
+flex: 1 280px;
 `;
 
 const News = styled.div`
+font-family: 'Raleway', sans-serif;
+padding: 10px;
 `;
 
 const Author = styled.div`
+font-family: 'Press Start 2P', cursive;
+padding: 10px;
 `;
 
 const Image = styled.div`
+  flex: 1 330px;
   height: 100%;
   object-fit: cover;
   background-image: url(${"https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"});
   background-position: center;
   background-size: cover;
-  broder-left-width: 100px;
+  broder-left-width: 1px;
   border-left-style: solid;
   border-left-color: ${(props) => props.color};
   /* position: relative; */
