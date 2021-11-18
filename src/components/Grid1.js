3fe5e54c3c2ad71bff9503c457d54c056f72f495
aritramullick <aritramullick@g.ucla.edu>
2021-11-17 16:24:15 -0800

    Added changes

--- src/components/Grid1.js
+++ src/components/Grid1.js
@@ -7,14 +7,15 @@ height: 50vh;
 /* width: ; */
 margin-left: 10%;
 margin-right: 10%;
-grid-template-columns: 10% auto auto 10%;
-grid-template-rows: auto auto auto auto auto;
+grid-template-columns: 10% 40% 40% 10%;
+grid-template-rows: auto auto auto auto auto auto;
 grid-template-areas: 
   "blue1 bigBlue bigRed red1"
   "blue2 bigBlue bigRed red2"
   "blue3 bigBlue bigRed red3"
   "blue4 bigBlue bigRed red4"
   "blue5 bigBlue bigRed red5"
+  "db-text db-text dt-text dt-text"
 `;
 
 const SmallImage = styled.div`
@@ -41,6 +42,24 @@ const BigImage = styled.div`
   justify-self: center;
 `;
 
+const DBText = styled.div`
+width: 95%;
+margin-left: 1%;
+color: black;
+background-color: red;
+font-family: 'Courier New', Courier, monospace;
+grid-area: db-text;
+`;
+
+const DTText = styled.div`
+width: 95%;
+margin-left: 4%;
+color: black;
+background-color: red;
+font-family: 'Courier New', Courier, monospace;
+grid-area: dt-text;
+`;
+
 export default function Grid1(){
     return (
         <Grid>
@@ -52,6 +71,18 @@ export default function Grid1(){
 
         <BigImage direction="bigBlue"/>
         <BigImage direction="bigRed"/>
+
+       <DBText>
+        LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
+         mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+        </DBText>
+
+        <DTText>
+        LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
+         mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+        </DTText>
+
+
 
         <SmallImage direction="red1"/>
         <SmallImage direction="red2"/>
