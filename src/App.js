ffb76f4411701e5790ee5b03cf1aaa5e80143250
aritramullick <aritramullick@g.ucla.edu>
2021-11-18 15:49:47 -0800

    Working styff

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import './App.css';
 import Grid1 from "./components/Grid1";
+import Grid1Mobile from "./components/Grid1Mobile";
+
 function App() {
   const [ data, setData ] = useState(null);
   
   useEffect(() => {
-		fetch("<TODO: insert api url here>")
+		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/interactive.2021.db-dt-football")
 		.then(res => res.json())
 		.then(res => setData(res.data['article.aml']))
   }, [])
 
   return (
     <div className="App">
-      {/* Hello Daily Bruin!
-       */}
-       <Grid1/>
+      {/* {console.log(data)} */}
+      <Grid1/>
+      
     </div>
   );
 }
--- src/components/Grid1.js
+++ src/components/Grid1.js
@@ -71,7 +71,7 @@ grid-template-areas:
 
 
 
-const SmallImage = styled.div`
+const SmallImag = styled.div`
   height: 90%;
   width: 90%;
   object-fit: cover;
@@ -81,6 +81,7 @@ const SmallImage = styled.div`
   grid-area: ${(props) => props.direction};
   justify-content: center;
   justify-self: center;
+  z-index: 2;
 `;
 
 const BigImage = styled.div`
@@ -102,6 +103,7 @@ color: black;
 background-color: red;
 font-family: 'Courier New', Courier, monospace;
 grid-area: ${(props) => props.direction};
+overflow-y:auto;
 `;
 
 const DTText = styled.div`
@@ -111,74 +113,158 @@ color: black;
 background-color: red;
 font-family: 'Courier New', Courier, monospace;
 grid-area: ${(props) => props.direction};
+overflow-y:auto;
 `;
 
 
-
-
 export default function Grid1(props) {
+  
     const [ DBText_, setDBText ] = useState(false);
     const [ DTText_, setDTText ] = useState(false);
     const [ UCLAimagePicked, setUCLAimagePicked ] = useState(0);
     const [ USCimagePicked, setUSCimagePicked ] = useState(0);
 
-    let UCLAsideSources = [];
-    let UCLAmainSources = [];
-    let UCLAplayerText = [];
-
-    let USCsideSources = [];
-    let USCmainSources = [];
-    let USCplayerText = [];
-
-    props.imageDetails.forEach((ele) => {
-      if (ele.id === "UCLA") {
-        UCLAsideSources.push(ele.src);
-        UCLAmainSources.push(ele.mainSrc);
-        UCLAplayerText.push(ele.playerText);
-      }
+    function SmallImage(props) {
+      let school = props.school;
+      let index = props.num;
+      let source = props.src;
+      let dir = props.direction;
+      if (school === "UCLA")
+        return (
+            <SmallImag src={source} direction = {dir} onClick={() => {console.log(index); setUCLAimagePicked(index)}}/>
+        )
       else {
-        USCsideSources.push(ele.src);
-        USCmainSources.push(ele.mainSrc);
-        USCplayerText.push(ele.playerText);
+        return (
+            <SmallImag src={source} direction = {dir} onClick={() => {console.log(index); setUSCimagePicked(index)}}/>
+        )
+
+      }
+    } 
+
+    let UCLAsideSources = [
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", 
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", 
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"];
+
+    let UCLAmainSources = ["https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", 
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", 
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"]
+
+    let UCLAplayerPreviewText = ["This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline"];
+
+    let UCLAplayerBigText = ["This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline"];
+
+    let USCsideSources = ["https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-19-5a3926af95d9d__700.jpg", 
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-19-5a3926af95d9d__700.jpg", 
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-19-5a3926af95d9d__700.jpg",
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-19-5a3926af95d9d__700.jpg",
+    "https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-19-5a3926af95d9d__700.jpg"];
+
+    let USCmainSources = ["https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", 
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", 
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg",
+    "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"];
+
+    let USCplayerPreviewText = [
+      "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline"
+    ];
+    let USCplayerBigText = [
+      "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline",
+    "This is text i am mind numbingly boring so i couldn't get a better headline"
+    ];
+
+    if (typeof(props.imageDetails) === 'undefined') {
+      console.log("HI");
+      ;
+    }
+
+    else {
+      console.log("WTF");
+        props.imageDetails.forEach((ele) => {
+          if (ele.id === "UCLA") {
+            UCLAsideSources.push(ele.src);
+            UCLAmainSources.push(ele.mainSrc);
+            UCLAplayerPreviewText.push(ele.playerPreviewText);
+            UCLAplayerBigText.push(ele.playerBigText);
+          }
+          else {
+            USCsideSources.push(ele.src);
+            USCmainSources.push(ele.mainSrc);
+            USCplayerPreviewText.push(ele.playerPreviewText);
+            USCplayerBigText.push(ele.playerBigText);
+          }
+
+        })
       }
 
-    })
-    let UCLASmallImages = []
-    let USCSmallImages = []
+    let UCLASmallImages = [];
+    let USCSmallImages = [];
 
-    for (var i = 0; i < UCLAsideSources.length; i++) {
+    for (var i = 0; i < UCLAsideSources.length; i=i+1) {
       let image_number = i+1;
-      UCLASmallImages.push(<SmallImage direction={"blue" + image_number} src={UCLAsideSources[i]} onClick={()=>{setUCLAimagePicked(i)}}/>);
+      UCLASmallImages.push(<SmallImage direction={"blue" + image_number} school= "UCLA" src={UCLAsideSources[i]} num={i}/>);
     }
 
-    for (var i = 0; i < USCsideSources.length; i++) {
-      let image_number = i+1;
-      UCLASmallImages.push(<SmallImage direction={"red" + image_number} src={USCsideSources[i]} onClick={()=>{setUSCimagePicked(i)}}/>);
+    
+
+    for (var j = 0; j < USCsideSources.length; j=j+1) {
+      let image_number = j+1;
+      USCSmallImages.push(<SmallImage direction={"red" + image_number} src={USCsideSources[j]} num={j}/>);
     }
 
-    let source = "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg";
+    /* console.log(UCLASmallImages);
+    console.log(USCSmallImages); */
+
 
     if (!DTText_ && !DBText_) {
           return (
               <NormalGrid>
-              {UCLASmallImages.map((ele, index) => {return ele})}
+              {UCLASmallImages.map((ele, index) => {
+                return (
+                    ele
+            )})}
 
               <BigImage direction="bigBlue" src={UCLAmainSources[UCLAimagePicked]}/>
+
               <BigImage direction="bigRed" src = {USCmainSources[USCimagePicked]}/>
 
             <DBText direction="db-text">
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+              {UCLAplayerPreviewText[UCLAimagePicked]}
               <div onClick={()=>{setDBText(true)}}> READ MORE</div>
               </DBText>
 
               <DTText direction="dt-text"t>
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+              {USCplayerPreviewText[USCimagePicked]}
               <div onClick={()=>{setDTText(true)}}> READ MORE</div>
               </DTText>
 
-              {USCSmallImages.map((ele, index) => {return ele})}
+              {USCSmallImages.map((ele, ind) => {
+                /* console.log(ind); */
+                return (
+                
+                    ele
+                
+            )})}
               </NormalGrid>
           );
     }
@@ -189,20 +275,22 @@ export default function Grid1(props) {
               <BigImage direction="bigRed" src = {USCmainSources[USCimagePicked]}/>
 
             <DBText direction = "left">
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+            {UCLAplayerBigText[UCLAimagePicked]}
               <div onClick={()=>{setDBText(false)}}> READ LESS</div>
               </DBText>
 
               <DTText direction="dt-text">
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+              {USCplayerPreviewText[USCimagePicked]}
               <div onClick={()=>{setDTText(true)}}> READ MORE</div>
               </DTText>
 
 
 
-              {USCSmallImages.map((ele, index) => {return ele})}
+              {USCSmallImages.map((ele, index) => {return (
+                <div onClick={() => {setUSCimagePicked(index)}}>
+                    {ele}
+                </div> )})}
+
               </LeftTextGrid>
           );
 
@@ -213,20 +301,27 @@ export default function Grid1(props) {
 
       return (
               <RightTextGrid>
-              {UCLASmallImages.map((ele, index) => {return ele})}
+              {UCLASmallImages.map((ele, index) => {return (
+                <div onClick={() => {console.log(index); setUCLAimagePicked(index)}}>
+                    {ele}
+                </div> 
+            )})}
 
               <BigImage direction="bigBlue" src={UCLAmainSources[UCLAimagePicked]}/>
               
 
             <DBText direction="db-text" >
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+            {UCLAplayerPreviewText[UCLAimagePicked]}
               <div onClick={()=>{setDBText(true)}}> READ MORE</div>
             </DBText>
 
               <DTText direction="right">
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+
+              {USCSmallImages.map((ele, index) => {return (
+                <div onClick={() => {setUSCimagePicked(index)}}>
+                    {ele}
+                </div> )})}
+
               <div onClick={()=>{setDTText(false)}}> READ LESS</div>
               </DTText>
 
@@ -239,14 +334,12 @@ export default function Grid1(props) {
               <BothSidesTextGrid>
               
             <DBText direction="left">
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+            {UCLAplayerBigText[UCLAimagePicked]}
               <div onClick={()=>{setDBText(false)}}> READ LESS</div>
               </DBText>
 
               <DTText direction="right">
-              LET US SEEEEEE lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend
-              mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum 
+              {USCplayerBigText[USCimagePicked]} 
               <div onClick={()=>{setDTText(false)}}> READ LESS</div>
               </DTText>
 
