eef38f2ab3e77ce6736c32a0639c514219067bb2
Sunny Li <lb@MacBook-Pro-11.local>
2021-11-17 22:16:13 -0800

    prob have to add a new file for mobile

--- src/components/Grid2.js
+++ src/components/Grid2.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import Grid2ArticleBox from '../components/Grid2ArticleBox'
+import {mediaQueries} from '../shared/config';
 
 const Grid = styled.div`
 display: grid;
 grid-template-columns: repeat(2, 1fr);
-grid-template-rows: auto;
+grid-gap: 5px;
+
 `;
 
 export default function Grid2() {
--- src/components/Grid2ArticleBox.js
+++ src/components/Grid2ArticleBox.js
@@ -4,8 +4,7 @@ import styled from 'styled-components';
 const Box = styled.div`
 display: flex;
 flex-direction: row;
-height: 95%;
-width: 85%;
+min-height: 200px;
 justify-content: center;
 align-items: center;
 `;
